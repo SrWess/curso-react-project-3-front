33673dd7c4d2ae435590390c06586297d381262f
Wesley Anjos <wanjos.andrade@gmail.com>
2021-07-14 11:02:03 -0300

    Configurado fontes e implementado breakpoint para Heading huge

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -12,8 +12,16 @@ const titleSize = {
   `,
   huge: (theme) => css`
     font-size: ${theme.font.sizes.xhuge};
+    ${mediaFont(theme)}
   `,
 };
+
+const mediaFont = (theme) => css`
+  @media ${theme.media.lteMedium} {
+    font-size: ${theme.font.sizes.xlarge};
+    background: purple;
+  }
+`;
 
 const titleCase = (uppercase) => css`
   text-transform: ${uppercase ? "uppercase" : "none"};
