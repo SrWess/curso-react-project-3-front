f45cc7a20b77a8eea272a4c5385a04d5952ba456
Wesley Anjos <wanjos.andrade@gmail.com>
2021-08-12 14:30:15 -0300

    Configurado testes para o componente GridTwoColumn

---
+++ src/components/GridTwoColumn/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'GridTwoColumn',
+  text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, blanditiis aliquam? Aliquam deleniti, exercitationem dolorem excepturi sequi odio similique praesentium molestias reprehenderit amet harum rerum recusandae quis eaque dolorum quibusdam!`,
+  srcImg: 'assets/images/javascript.svg',
+}
--- src/components/GridTwoColumn/stories.jsx
+++ src/components/GridTwoColumn/stories.jsx
@@ -1,13 +1,11 @@
 import { GridTwoColumn } from '.';
 
+import mock from './mock';
+
 export default {
   title: 'GridTwoColumn',
   component: GridTwoColumn,
-  args: {
-    title: 'GridTwoColumn',
-    text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, blanditiis aliquam? Aliquam deleniti, exercitationem dolorem excepturi sequi odio similique praesentium molestias reprehenderit amet harum rerum recusandae quis eaque dolorum quibusdam!`,
-    srcImg: 'assets/images/javascript.svg',
-  },
+  args: mock,
   argTypes: {
     children: { type: 'string' },
   },
