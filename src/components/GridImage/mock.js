7c180d1851929492379f59e8260afdc236c3826b
Wesley Anjos <wanjos.andrade@gmail.com>
2021-08-13 11:41:10 -0300

    Criado componente GridImage e Testes

---
+++ src/components/GridImage/mock.js
@@ -0,0 +1,32 @@
+export default {
+  background: false,
+  title: 'My grid image',
+  description:
+    'Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde',
+  grid: [
+    {
+      altText: 'Teste 1',
+      srcImg: 'https://dummyimage.com/800x800/000/fff',
+    },
+    {
+      altText: 'Teste 2',
+      srcImg: 'https://dummyimage.com/800x800/000/fff',
+    },
+    {
+      altText: 'Teste 3',
+      srcImg: 'https://dummyimage.com/800x800/000/fff',
+    },
+    {
+      altText: 'Teste 4',
+      srcImg: 'https://dummyimage.com/800x800/000/fff',
+    },
+    {
+      altText: 'Teste 5',
+      srcImg: 'https://dummyimage.com/800x800/000/fff',
+    },
+    {
+      altText: 'Teste 6',
+      srcImg: 'https://dummyimage.com/800x800/000/fff',
+    },
+  ],
+};
