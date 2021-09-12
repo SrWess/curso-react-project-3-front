b0cf3110153c2c053e31b8ad90b7fb8e0f4ddfc0
Wesley Anjos <wanjos.andrade@gmail.com>
2021-09-12 13:59:10 -0300

    Carregando dados da API e implementado validação de loading e page not found

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+import { Base } from '../Base/index';
+import { mockBase } from '../Base/mock';
+
+import { mapData } from '../../api/map-data';
+import { PageNotFound } from '../PageNotFound';
+
+function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetch(
+          'http://localhost:1337/pages/?slug=landing-pages',
+        );
+        const json = await data.json();
+        const pageData = mapData(json);
+
+        setData(pageData[0]);
+      } catch (e) {
+        setData(undefined);
+      }
+    };
+
+    load();
+  }, []);
+
+  if (data === undefined) {
+    return <PageNotFound/>;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>;
+  }
+
+  return <Base {...mockBase} />;
+}
+
+export default Home;
