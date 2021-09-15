6fd7bbd16b64cb0a6e98c02033b5c987d0ac9064
Wesley Anjos <wanjos.andrade@gmail.com>
2021-09-15 18:03:41 -0300

    Iniciado procedimento de distribuição dos dados para os componentes

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react';
 
-import { Base } from '../Base/index';
-import { mockBase } from '../Base/mock';
-
 import { mapData } from '../../api/map-data';
+
+import { GridTwoColumn } from '../../components/GridTwoColumn';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+
+import { Base } from '../Base/index';
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
+import { mockBase } from '../Base/mock';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -29,14 +34,47 @@ function Home() {
   }, []);
 
   if (data === undefined) {
-    return <PageNotFound/>;
+    return <PageNotFound />;
   }
 
   if (data && !data.slug) {
     return <Loading />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      logoData={{ text, link, srcImg }}
+      footerHtml={footerHtml}
+    >
+      {
+        sections.map((section, index) => {
+          const { component } = section
+          const key = `${slug}-${index}`;
+
+          if (component === 'section.section-two-columns') {
+            return <GridTwoColumn key={key} {...section} />;
+          }
+
+          if (component === 'section.section-content') {
+            return <GridContent key={key} {...section} />;
+          }
+
+          if (component === 'section.section-grid-text') {
+            return <GridText key={key} {...section} />;
+          }
+
+          if (component === 'section.section-grid-image') {
+            return <GridImage key={key} {...section} />;
+          }
+        })
+      }
+    </Base>
+  );
 }
 
 export default Home;
